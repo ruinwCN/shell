533d852ea526dd964c9ba0fdc7414bebe7809e8d
ruinwCN <ruinwcn@gmail.com>
2021-12-20 21:30:30 +0800

    add : docker install

---
+++ docker_install.sh
@@ -0,0 +1,12 @@
+echo "install ..."
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+yes | sudo apt install docker-ce
+apt list -a docker-ce
+yes | sudo apt install docker-ce=5:18.09.6~3-0~ubuntu-bionic
+# hold version
+sudo apt-mark hold docker-ce
+sudo usermod -aG docker $USER
